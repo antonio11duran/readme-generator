3d1c91f7e6da27e710f63e8333517bf66f8b1227
Duran11 <antonio11duran@gmail.com>
2022-10-18 12:06:18 -0600

    fixed array issue, licenses now are added to readme, need to check licenses that have spaces

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,92 +1,67 @@
-const licenseArray = [
-  {
-    name: "Apache",
-    badgeUrl: "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
-    link: "https://choosealicense.com/licenses/apache-2.0/",
-  },
-  {
-    name: "GNU GPL 3.0",
-    badgeUrl: "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
-    link: "https://choosealicense.com/licenses/gpl-3.0/",
-  },
-  {
-    name: "GNU GPL 2.0",
-    badgeUrl: "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)",
-    link: "https://choosealicense.com/licenses/gpl-2.0/",
-  },
-  {
-    name: "GNU LGPL",
-    badgeUrl: "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)",
-    link: "https://choosealicense.com/licenses/lgpl-3.0/",
-  },
-  {
-    name: "MIT",
-    badgeUrl: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
-    link: "https://choosealicense.com/licenses/mit/",
-  },
-  {
-    name: "BSD 2",
-    badgeUrl: "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)",
-    link: "https://choosealicense.com/licenses/bsd-2-clause/",
-  },
-  {
-    name: "BSD 3",
-    badgeUrl: "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
-    link: "https://choosealicense.com/licenses/bsd-3-clause/",
-  },
-  {
-    name: "Boost",
-    badgeUrl: "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)",
-    link: "https://choosealicense.com/licenses/bsl-1.0/",
-  },
-  {
-    name: "Creative Commons",
-    badgeUrl: "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)",
-    link: "https://choosealicense.com/licenses/cc0-1.0/",
-  },
-  {
-    name: "Eclipse",
-    badgeUrl: "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)",
-    link: "https://choosealicense.com/licenses/epl-1.0/",
-  },
-  {
-    name: "GNU Affero",
-    badgeUrl: "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)",
-    link: "https://choosealicense.com/licenses/agpl-3.0/",
-  },
-  {
-    name: "Mozilla",
-    badgeUrl: "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
-    link: "https://choosealicense.com/licenses/mpl-2.0/",
-  },
-  {
-    name: "Unlicense",
-    badgeUrl: "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)",
-    link: "https://choosealicense.com/licenses/unlicense/",
-  }
-]
+// need to fix how array is set up. when pulling name from array, it comes back as undefined
+const names = ["Apache",
+  "GNU GPL 3.0",
+  "GNU GPL 2.0",
+  "GNU LGPL",
+  "MIT",
+  "BSD 2",
+  "BSD 3",
+  "Boost",
+  "Creative Commons",
+  "Eclipse",
+  "GNU Affero",
+  "Mozilla",
+  "Unlicense"]
 
-const { name, badgeUrl, link } = licenseArray
+const badgeUrls = ["[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+  "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+  "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)",
+  "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)",
+  "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+  "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)",
+  "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
+  "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)",
+  "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)",
+  "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)",
+  "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)",
+  "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
+  "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"]
+
+const links = ["https://choosealicense.com/licenses/apache-2.0/",
+  "https://choosealicense.com/licenses/gpl-3.0/",
+  "https://choosealicense.com/licenses/gpl-2.0/",
+  "https://choosealicense.com/licenses/lgpl-3.0/",
+  "https://choosealicense.com/licenses/mit/",
+  "https://choosealicense.com/licenses/bsd-2-clause/",
+  "https://choosealicense.com/licenses/bsd-3-clause/",
+  "https://choosealicense.com/licenses/bsl-1.0/",
+  "https://choosealicense.com/licenses/cc0-1.0/",
+  "https://choosealicense.com/licenses/epl-1.0/",
+  "https://choosealicense.com/licenses/agpl-3.0/",
+  "https://choosealicense.com/licenses/mpl-2.0/",
+  "https://choosealicense.com/licenses/unlicense/"]
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  console.log(license);
-  console.log(name);
-  if (license == name) {
-    return badgeUrl;
-  } else {
-    return "";
+  for (let i = 0; i < names.length; i++) {
+    if (license == names[i]) {
+      return badgeUrls[i];
+    } else {
+      return "";
+    }
   }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license == name) {
-    return link;
-  } else {
-    return "No License";
+  for (let i = 0; i < names.length; i++) {
+    if (license == names[i]) {
+      return links[i];
+    } else {
+      return "No License";
+    }
   }
 }
 
